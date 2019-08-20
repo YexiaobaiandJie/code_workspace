66473e10a7473cb231a9abaf1c00f002513003a0
helijie <1124924603@qq.com>
2019-08-20 20:34:42 +0800

    修改了主页样式

--- code_home/src/components/HelloWorld.vue
+++ code_home/src/components/HelloWorld.vue
@@ -1,90 +1,28 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <!--
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-    -->
-    <input type="text" placeholder="username" class="login_input" id="userid"/>
-    <input type="text" placeholder="password" class="login_input" id="pwd"/>
-    <button v-on:click="login">LOGIN</button>
+    <div>
+      <h1>{{ '-WELCOME-' }}{{id_input}}</h1>
+      <el-input v-model="id_input" placeholder="username" id="userid" class="login_input"></el-input>
+      <el-input v-model="pwd_input" placeholder="password" id="pwd" class="login_input"></el-input>
+      <el-button type="primary" plain :disabled="true" icon="el-icon-user">LOGIN</el-button>
+    </div>
+    <div style="margin-top:50px;display:flex;justify-content:center;align-items:center;">
+      <div class="index_function_light">
+      <p class="index_font">blue</p>
+      </div>
+      <div class="index_function2_light">
+      <p class="index_font">green</p>
+      </div>
+      <div class="index_function3_light">
+      <p class="index_font">orange</p>
+      </div>
+      <div class="index_function4_light">
+      <p class="index_font">red</p>
+      </div>
+      <div class="index_function5_light">
+      <p class="index_font">gray</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -93,7 +31,8 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      id_input: '',
+      pwd_input: '',
     }
   },
   methods: {
@@ -102,13 +41,15 @@ export default {
       var pwdinput = document.getElementById('pwd')
       var userid = idinput.value
       var pwd = pwdinput.value
-      this.$http.post('http://localhost:8360/login', {'Userid': userid, 'Password': pwd}).then(function(res)
+      this.$http.post('http://localhost:8360/index/login', {'Userid': userid, 'Password': pwd}).then(function(res)
       { 
         var data = res.body
         console.log(data)
       })
     }
+    
   }
+
 }
 </script>
 
@@ -133,6 +74,73 @@ a {
   display:block;
   margin-left:auto;
   margin-right:auto;
+  width:500px;
+}
+.index_function_heavy{
+  width:220px;
+  height:356px;
+  border-radius:0px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
 }
 
+.index_function_light{
+  width:200px;
+  height:324px;
+  border-radius:0px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color:#409EFF;
+  float:left;
+  margin-left:10px;
+  margin-right:10px;
+}
+
+.index_function2_light{
+  width:200px;
+  height:324px;
+  border-radius:0px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color:#67C23A;
+  float:left;
+  margin-left:10px;
+  margin-right:10px;
+}
+.index_function3_light{
+  width:200px;
+  height:324px;
+  border-radius:0px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color:#E6A23C;
+  float:left;
+  margin-left:10px;
+  margin-right:10px;
+}
+.index_function4_light{
+  width:200px;
+  height:324px;
+  border-radius:0px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color:#F56C6C;
+  float:left;
+  margin-left:10px;
+  margin-right:10px;
+}
+.index_function5_light{
+  width:200px;
+  height:324px;
+  border-radius:0px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color:#909399;
+  float:left;
+  margin-left:10px;
+  margin-right:10px;
+}
+.index_font{
+  font-family:PingFang SC;
+  font-weight:800;
+  font-size:30px;
+  color:#fff;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+}
 </style>
--- code_home/src/main.js
+++ code_home/src/main.js
@@ -4,13 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
+Vue.use(Element,{ size:'small',zIndex:3000})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+ // render:h => h(APP),
   router,
   components: { App },
   template: '<App/>'

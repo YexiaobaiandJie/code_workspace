21a948f5588e07a1afc9ed7db03719c8ee372e71
helijie <1124924603@qq.com>
2019-08-20 17:29:56 +0800

    vue_page

---
+++ code_home/src/components/HelloWorld.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <!--
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+    -->
+    <input type="text" placeholder="username" class="login_input" id="userid"/>
+    <input type="text" placeholder="password" class="login_input" id="pwd"/>
+    <button v-on:click="login">LOGIN</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    login: function () {
+      var idinput = document.getElementById('userid')
+      var pwdinput = document.getElementById('pwd')
+      var userid = idinput.value
+      var pwd = pwdinput.value
+      this.$http.post('http://localhost:8360/login', {'Userid': userid, 'Password': pwd}).then(function(res)
+      { 
+        var data = res.body
+        console.log(data)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.login_input {
+  margin:20px;
+  display:block;
+  margin-left:auto;
+  margin-right:auto;
+}
+
+</style>
